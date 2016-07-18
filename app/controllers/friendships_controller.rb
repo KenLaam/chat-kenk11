2bc73881265f8affe739a96719bd0b77f44f15c4
kenk11 <hoangvu23108@gmail.com>
2016-07-18 23:24:35 +0700

    finish remove friend

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -10,13 +10,13 @@ class FriendshipsController < ApplicationController
       redirect_to friends_path
     else
       flash[:error] = "Error: #{@friend.errors.full_messages.to_sentence}"
+      redirect_to friends_path
     end
   end
 
   def destroy
-    @friendship = current_user.friendships.find params[:friend_id]
-    @friend = @friendship.friend.find params[:friend_id]
-    @friend.destroy
+    @friendship = current_user.friendships.where(friend_id: params[:friend_id]).first
+    @friendship.destroy
     flash[:success] = 'Removed friend successful!'
     redirect_to friends_path
   end
